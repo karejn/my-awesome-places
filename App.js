ef38e3499480f8958038359863b47958a6f365d5
Ekaterina <ek.zhuravleva@gmail.com>
2019-04-07 11:40:16 +0200

    Basic setup for the app.

---
+++ App.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import placeImage from "./src/assets/claddagh.jpg";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
+
+export default class App extends Component {
+  state = {
+    places: [],
+    selectedPlace: null
+  };
+
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat({
+          key: Math.random(),
+          name: placeName,
+          image: {
+            uri: "https://media-cdn.tripadvisor.com/media/photo-s/07/09/5f/0b/the-claddagh.jpg"
+          }
+        })
+      };
+    });
+  };
+  
+  placeDeletedHandler = () => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter(place => {
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    });
+  }
+
+  modalClosedHanler = () => {
+    this.setState({
+      selectedPlace: null
+    });
+  }
+
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+        })
+      }
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceDetail selectedPlace={this.state.selectedPlace} onItemDeleted={this.placeDeletedHandler} onModalClosed={this.modalClosedHanler}/>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.state.places}
+          onItemSelected={this.placeSelectedHandler}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
+});
