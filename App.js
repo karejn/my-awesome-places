acff57ca5ae6569ec7288a6d4118a1ab497e4c4b
Ekaterina <ek.zhuravleva@gmail.com>
2019-04-10 08:26:26 +0200

    Updating redux.

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import AuthScreen from './src/screens/Auth/Auth';
 import ScharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import configureStore from './src/store/configureStore';
 
+const store = configureStore();
 //Register screens
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
-Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
 
 //Start an App
 Navigation.startSingleScreenApp({
