7a25cb678c5a5a3637fa28abc82342e89002fa0e
Ekaterina <ek.zhuravleva@gmail.com>
2019-04-14 13:35:31 +0200

    Configuring text input components.

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -142,7 +142,8 @@ class AuthScreen extends Component {
                         value={this.state.controls.confirmPassword.value}
                         onChangeText={val => this.updateInputState('confirmPassword', val)} 
                         valid={this.state.controls.confirmPassword.valid}
-                        touched={this.state.controls.confirmPassword.touched} />
+                        touched={this.state.controls.confirmPassword.touched}
+                        secureTextEntry />
                 </View>
                 );
             }
@@ -162,7 +163,10 @@ class AuthScreen extends Component {
                                 value={this.state.controls.email.value}
                                 onChangeText={val => this.updateInputState('email', val)} 
                                 valid={this.state.controls.email.valid}
-                                touched={this.state.controls.email.touched} />
+                                touched={this.state.controls.email.touched} 
+                                autoCapitalize="none"
+                                autoCorrect={false} 
+                                keyboardType="email-address" />
                             <View style={this.state.viewMode === "portrait" || this.state.authMode === "login"
                                 ? styles.portraitPasswordContainer 
                                 : styles.landscapePasswordContainer}>
@@ -175,7 +179,8 @@ class AuthScreen extends Component {
                                         value={this.state.controls.password.value}
                                         onChangeText={val => this.updateInputState('password', val)}
                                         valid={this.state.controls.password.valid}
-                                        touched={this.state.controls.password.touched} />
+                                        touched={this.state.controls.password.touched}
+                                        secureTextEntry />
                                 </View>
                                 {confrimPasswordControl}
                             </View>
