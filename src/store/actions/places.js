554da167a2cb7bf7003412315239f4cc5f5776ad
Ekaterina <ek.zhuravleva@gmail.com>
2019-04-28 11:14:15 +0200

    Image upload function.

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -6,15 +6,26 @@ export const addPlace = (placeName, location, image) => {
             name: placeName,
             location: location
         };
-        fetch("https://awesome-places-1555851310405.firebaseio.com/places.json", {
+        fetch("https://us-central1-awesome-places-1555851310405.cloudfunctions.net/storeImage", {
             method: "POST",
-            body: JSON.stringify(placeData)
+            body: JSON.stringify({
+                image: image.base64
+            })
         })
         .catch(err => console.log(err))
         .then(res => res.json())
         .then(parsedRes => {
             console.log(parsedRes);
         });
+        // fetch("https://awesome-places-1555851310405.firebaseio.com/places.json", {
+        //     method: "POST",
+        //     body: JSON.stringify(placeData)
+        // })
+        // .catch(err => console.log(err))
+        // .then(res => res.json())
+        // .then(parsedRes => {
+        //     console.log(parsedRes);
+        // });
     };
 };
 
